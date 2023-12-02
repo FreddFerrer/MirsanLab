bb9d9132ad56bcfd57d0b6d9c46a28620c77c467
Freddy <ferreirafreddy39@gmail.com>
2023-12-01 21:58:24 -0300

    cards added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/index/components/header/header.component';
 import { HomeComponent } from './pages/index/home/home.component';
+import { FooterComponent } from './pages/index/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
